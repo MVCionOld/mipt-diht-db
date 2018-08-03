9cde664db3ddead18eb6eeea010e0b9d8bdf6c36
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 21:01:28 +0300

    Update 018.sql

--- sqlex/018.sql
+++ sqlex/018.sql
@@ -1,5 +1,20 @@
 /* LANGUAGE plpgsql */
-SELECT DISTINCT maker, price
-FROM Printer JOIN Product
-ON Printer.model = Product.model
-WHERE price =(SELECT MIN (price) FROM Printer WHERE color = 'y') AND color = 'y'
+SELECT DISTINCT 
+    maker
+    , price
+FROM
+    Printer 
+JOIN
+    Product
+    ON
+        Printer.model=Product.model
+WHERE 1=1
+    AND price=(
+        SELECT 
+            MIN(price)
+        FROM
+            Printer 
+        WHERE 1=1
+            AND color='y'
+    ) 
+    AND color='y'
