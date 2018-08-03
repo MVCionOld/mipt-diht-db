2b2ea6c560a024ae4eae68e3951f8d5564266584
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 20:48:54 +0300

    Update 009.sql

--- sqlex/009.sql
+++ sqlex/009.sql
@@ -1,5 +1,11 @@
 /* LANGUAGE plpgsql */
-SELECT DISTINCT maker
-FROM Product JOIN PC
-ON Product.model = PC.model
-WHERE speed >=450
+SELECT DISTINCT 
+    maker
+FROM 
+    Product 
+JOIN 
+    PC
+    ON 
+        Product.model = PC.model
+WHERE 1=1
+    AND speed>=450
