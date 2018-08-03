e69e0fb617e91e2125591ba468d07162efbf4376
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 20:42:42 +0300

    Update 007.sql

--- sqlex/007.sql
+++ sqlex/007.sql
@@ -1,15 +1,40 @@
 /* LANGUAGE plpgsql */
-SELECT Product.model, price
-FROM Product JOIN PC
-ON Product.model = PC.model
-WHERE maker = 'B'
+SELECT 
+    Product.model
+    , price
+FROM 
+    Product 
+JOIN 
+    PC
+    ON 
+        Product.model=PC.model
+WHERE 1=1
+    AND maker='B'
+    
 UNION
-SELECT Product.model, price
-FROM Product JOIN Laptop
-ON Product.model = Laptop.model
-WHERE maker = 'B'
+
+SELECT
+    Product.model
+    , price
+FROM
+    Product
+JOIN 
+    Laptop
+    ON 
+        Product.model=Laptop.model
+WHERE 1=1
+  AND maker='B'
+  
 UNION
-SELECT Product.model, price
-FROM Product JOIN Printer
-ON Product.model = Printer.model
-WHERE maker = 'B'
+
+SELECT 
+    Product.model
+    , price
+FROM 
+    Product 
+JOIN 
+    Printer
+    ON 
+        Product.model=Printer.model
+WHERE 1=1
+    AND maker='B'
