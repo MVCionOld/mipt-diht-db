988815fc0daede909ed64006f304e906d82ffa76
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 20:58:54 +0300

    Update 017.sql

--- sqlex/017.sql
+++ sqlex/017.sql
@@ -1,6 +1,17 @@
 /* LANGUAGE plpgsql */
-SELECT DISTINCT Product.type, Laptop.model, Laptop.speed
-FROM Laptop, PC, Product
-WHERE Laptop.speed < ALL
-	(SELECT speed
-	FROM PC) AND Product.type = 'Laptop'
+SELECT DISTINCT 
+    Product.type
+	, Laptop.model
+	, Laptop.speed
+FROM 
+	Laptop
+	, PC
+	, Product
+WHERE 1=1
+	AND Laptop.speed<ALL(
+		SELECT 
+			speed
+		FROM 
+			PC
+	) 
+	AND Product.type='Laptop'
