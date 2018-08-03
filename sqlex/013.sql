f1ad28ec1cb05d3ecd4f4d8bd208f6c795dec01e
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 20:52:28 +0300

    Update 013.sql

--- sqlex/013.sql
+++ sqlex/013.sql
@@ -1,5 +1,10 @@
 /* LANGUAGE plpgsql */
-SELECT AVG(speed)
-FROM Product JOIN PC
-ON Product.model = PC.model
-WHERE maker = 'A'
+SELECT 
+    AVG(speed)
+FROM
+    Product 
+JOIN PC
+    ON 
+        Product.model=PC.model
+WHERE 1=1
+    AND maker='A'
