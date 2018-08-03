08b7cece709d7a8f3a2bb2ddd8818af8dff8ba43
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 20:56:36 +0300

    Update 016.sql

--- sqlex/016.sql
+++ sqlex/016.sql
@@ -1,4 +1,13 @@
 /* LANGUAGE plpgsql */
-SELECT DISTINCT a.model, b.model, a.speed, a.RAM
-FROM pc AS  a, pc AS b
-WHERE a.ram = b.ram and a.speed = b.speed and a.model > b.model
+SELECT DISTINCT 
+    a.model
+    , b.model
+    , a.speed
+    , a.RAM
+FROM 
+  pc AS a
+  , pc AS b
+WHERE 1=1
+    AND a.ram=b.ram 
+    AND a.speed=b.speed
+    AND a.model>b.model
