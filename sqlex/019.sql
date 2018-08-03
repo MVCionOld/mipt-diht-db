910c78a392f2d73c0a33540b9ea42c1a7a415969
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 21:03:02 +0300

    Update 019.sql

--- sqlex/019.sql
+++ sqlex/019.sql
@@ -1,6 +1,21 @@
 /* LANGUAGE plpgsql */
-SELECT maker, AVG (screen) AS 'Average Screen'
-FROM Product JOIN Laptop
-ON Product.model = Laptop.model
-WHERE Laptop.model IN (SELECT model FROM Product WHERE type = 'Laptop')
-GROUP BY maker
+SELECT 
+    maker
+    , AVG(screen) AS 'Average Screen'
+FROM 
+    Product 
+JOIN 
+    Laptop
+    ON 
+        Product.model=Laptop.model
+WHERE 1=1
+    AND Laptop.model IN (
+        SELECT 
+            model 
+        FROM 
+            Product 
+        WHERE 1=1
+            AND type='Laptop'
+    )
+GROUP BY
+    maker
