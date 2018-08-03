25dd4254941eadcb58d353e941b98e87b28503e9
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-03 20:43:37 +0300

    Update 008.sql

--- sqlex/008.sql
+++ sqlex/008.sql
@@ -1,8 +1,16 @@
 /* LANGUAGE plpgsql */
-SELECT maker
-FROM Product
-WHERE type = 'PC'
+SELECT 
+    maker
+FROM 
+    Product
+WHERE 1=1 
+    type='PC'
+    
 EXCEPT
-SELECT maker
-FROM Product
-WHERE type = 'Laptop'
+
+SELECT 
+    maker
+FROM 
+    Product
+WHERE 1=1
+    AND type='Laptop'
