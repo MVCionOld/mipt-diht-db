bddcd669490480c9563e7408cd1b86b7508aad08
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-04 12:42:51 +0300

    Update 027.sql

--- sqlex/027.sql
+++ sqlex/027.sql
@@ -1,5 +1,21 @@
 /* LANGUAGE plpgsql */
-SELECT maker, AVG(hd) AS Avg_hd
-FROM pc inner join Product on pc.model = product.model
-GROUP BY maker
-HAVING maker IN (SELECT maker FROM Product WHERE type = 'printer')
+SELECT 
+    maker
+    , AVG(hd) AS Avg_hd
+FROM 
+    PC 
+INNER JOIN 
+    Product 
+    ON 
+        PC.model=Product.model
+GROUP BY 
+    maker
+HAVING 
+    maker IN (
+        SELECT 
+            maker 
+        FROM 
+            Product 
+        WHERE 1=1
+            AND type='printer'
+    )
