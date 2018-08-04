cc5a263975d670045035ba64e99fe80ec6dde8a8
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-04 12:40:41 +0300

    Update 026.sql

--- sqlex/026.sql
+++ sqlex/026.sql
@@ -1,11 +1,28 @@
 /* LANGUAGE plpgsql */
-SELECT AVG(price)
-FROM(
-	SELECT price
-	FROM PC inner join Product on pc.model = product.model
-	WHERE maker = 'A'
+SELECT 
+	AVG(price)
+FROM (
+	SELECT 
+		price
+	FROM 	
+		PC 
+	INNER JOIN 
+		Product 
+		ON 
+			PC.model=Product.model
+	WHERE 1=1
+		AND maker='A'
+	
 	UNION ALL
-	SELECT price
-	FROM Laptop inner join Product on Laptop.model = product.model
-	WHERE maker = 'A'
+	
+	SELECT 
+		price
+	FROM 
+		Laptop 
+	INNER JOIN 
+		Product 
+		ON 
+			Laptop.model=Product.model
+	WHERE 1=1
+		AND maker = 'A'
 ) AS pr
