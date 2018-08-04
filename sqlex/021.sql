70f688a8380d30fb25e3964721c05bf156af425b
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-04 12:27:40 +0300

    Update 021.sql

--- sqlex/021.sql
+++ sqlex/021.sql
@@ -1,5 +1,12 @@
 /* LANGUAGE plpgsql */
-SELECT maker, MAX(price) AS Max_price
-FROM PC JOIN Product
-ON PC.model = Product.model
-GROUP BY maker
+SELECT 
+    maker
+    , MAX(price) AS Max_price
+FROM 
+    PC 
+JOIN 
+    Product
+    ON 
+        PC.model = Product.model
+GROUP BY 
+    maker
