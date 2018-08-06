9055fd3b57dcbccaa57f41a42bbfd58e67d89959
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-06 23:13:48 +0300

    Update 032.sql

--- sqlex/032.sql
+++ sqlex/032.sql
@@ -1,13 +1,22 @@
 /* LANGUAGE plpgsql */
-SELECT DISTINCT Q.country, CONVERT(NUMERIC(6,2), (SUM((Q.bore * Q.bore * Q.bore) * Q.cnt) / SUM(Q.cnt) / 2)) weight
-FROM (SELECT c.country, c.class, c.bore, (
+WITH Q AS (
+    SELECT C.country, C.class, C.bore, (
           SELECT (COUNT(name)+
                  (SELECT COUNT(T.ship) 
                   FROM (SELECT DISTINCT ship 
                         FROM Outcomes 
-                        WHERE ship = c.class AND ship NOT IN (SELECT name FROM Ships)) T )) cnt       
+                        WHERE ship = C.class AND ship NOT IN (SELECT name FROM Ships)) T )) cnt       
           FROM Ships 
-          WHERE class = c.class) cnt
-     FROM classes c) Q
-WHERE Q.cnt > 0
-GROUP BY Q.country
+          WHERE class = C.class) cnt
+     FROM Classes C
+)
+
+SELECT DISTINCT 
+    Q.country
+    , CONVERT(NUMERIC(6,2), (SUM((Q.bore * Q.bore * Q.bore) * Q.cnt) / SUM(Q.cnt) / 2)) weight
+FROM  
+    Q
+WHERE 1=1 
+    AND Q.cnt>0
+GROUP BY 
+    Q.country
