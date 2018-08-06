1bcae43e61c2075994b123670717f32388f62900
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-06 23:06:09 +0300

    Update 034.sql

--- sqlex/034.sql
+++ sqlex/034.sql
@@ -1,4 +1,11 @@
 /* LANGUAGE plpgsql */
-SELECT name
-FROM Ships, Classes 
-WHERE Ships.class = Classes.class AND launched >= 1922 AND type = 'bb' AND displacement > 35000
+SELECT 
+    name
+FROM 
+    Ships
+    , Classes 
+WHERE 1=1
+    Ships.class=Classes.class
+    AND launched>=1922
+    AND type = 'bb'
+    AND displacement>35000
