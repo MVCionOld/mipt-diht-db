696592bcb8e800e22e56a07e215ead870864d803
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-06 23:22:26 +0300

    Update 039.sql

--- sqlex/039.sql
+++ sqlex/039.sql
@@ -1,14 +1,32 @@
 /* LANGUAGE plpgsql */
-WITH t1 AS (SELECT ship, date
-FROM outcomes, battles
-WHERE outcomes.battle = battles.name
-AND outcomes.result = 'damaged')
-,
-t2 AS (SELECT ship, date
-FROM outcomes, battles
-WHERE outcomes.battle = battles.name
+WITH 
+t1 AS (
+    SELECT 
+        ship
+        , date
+    FROM 
+        Outcomes
+        , Battles
+    WHERE 1=1
+        AND Outcomes.battle=Battles.name
+        AND Outcomes.result='damaged'
+)
+, t2 AS (
+    SELECT 
+        ship
+        , date
+    FROM 
+        Outcomes
+        , Battles
+    WHERE 1=1
+        AND Outcomes.battle=Battles.name
 )
 
-SELECT DISTINCT t2.ship
-FROM t1, t2
-WHERE t1.ship = t2.ship AND t1.date < t2.date
+SELECT DISTINCT 
+    t2.ship
+FROM 
+    t1
+    , t2
+WHERE 1=1
+    AND t1.ship=t2.ship 
+    AND t1.date<t2.date
