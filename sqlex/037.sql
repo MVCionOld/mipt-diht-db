035d03bbb55be367f5f020d792d421cbeea7ba4b
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-06 23:18:54 +0300

    Update 037.sql

--- sqlex/037.sql
+++ sqlex/037.sql
@@ -1,18 +1,38 @@
 /* LANGUAGE plpgsql */
-WITH t1 AS (SELECT class, name
-FROM ships
-UNION ALL
-SELECT outcomes.ship, outcomes.ship
-FROM outcomes, classes
-WHERE outcomes.ship = classes.class
+WITH 
+t1 AS (
+    SELECT 
+        class
+        , name
+    FROM 
+        Ships
+
+    UNION ALL
+
+    SELECT 
+        Outcomes.ship
+        , Outcomes.ship
+    FROM 
+        Outcomes
+        , Classes
+    WHERE 
+        Outcomes.ship=Classes.class
 )
-,
-t2 AS (
-SELECT * FROM t1
-GROUP BY class, name
+, t2 AS (
+    SELECT 
+        * 
+    FROM 
+        t1
+    GROUP BY
+        class
+        , name
 )
 
-SELECT class 
-FROM t2
-GROUP BY class
-HAVING count(1) = 1
+SELECT 
+    class 
+FROM 
+    t2
+GROUP BY
+    class
+HAVING
+    COUNT(1)=1 /* dirty hack))0) */
