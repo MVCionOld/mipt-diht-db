f143f0c0a2279e2581df73f4cadc0985583c20ef
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-06 23:23:27 +0300

    Update 040.sql

--- sqlex/040.sql
+++ sqlex/040.sql
@@ -1,5 +1,13 @@
 /* LANGUAGE plpgsql */
-SELECT Distinct Ships.class, name, country
-FROM Classes JOIN Ships
-ON Classes.class = Ships.class
-WHERE numGuns >= 10
+SELECT DISTINCT
+    Ships.class
+    , name
+    , country
+FROM 
+    Classes 
+JOIN 
+    Ships
+    ON 
+        Classes.class=Ships.class
+WHERE 
+    numGuns>=10
