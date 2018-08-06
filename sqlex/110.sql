2912feb04ad79de4a9724ada0f2dd3797b581bf3
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-08-06 23:29:32 +0300

    Update 110.sql

--- sqlex/110.sql
+++ sqlex/110.sql
@@ -1,5 +1,21 @@
 /* LANGUAGE plpgsql */
-SELECT (SELECT name FROM Passenger WHERE ID_psg = T.ID_psg) AS name
-FROM (SELECT DISTINCT ID_psg
-FROM Pass_in_trip AS PIT INNER JOIN Trip ON PIT.trip_no = Trip.trip_no
-WHERE Trip.time_out > Trip.time_in AND DATENAME(weekday, date) = 'Saturday') AS T
+SELECT (
+    SELECT 
+        name 
+    FROM 
+        Passenger 
+    WHERE 
+        ID_psg=T.ID_psg
+) AS name
+FROM (
+    SELECT DISTINCT 
+        ID_psg
+    FROM 
+        Pass_in_trip AS PIT
+    JOIN Trip 
+        ON 
+            PIT.trip_no=Trip.trip_no
+    WHERE 1=1
+          AND Trip.time_out>Trip.time_in 
+          AND DATENAME(weekday, date)='Saturday'
+) AS T
